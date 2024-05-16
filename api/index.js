327f0860b451dbdf951341105c67ef2a02a1b5ba
Sumit Gupta <sg065387@gmail.com>
2024-05-17 03:59:28 +0545

    connection to mongodb

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/',(req, res)=>{
-console.log("Ding!");
+try {
+
+    await mongoose.connect(process.env.MONGO);
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}...`);
+    })
+}
+catch (error) {
+    console.log(error.message);
+}
+
+app.get('/', (req, res) => {
+    console.log("Ding!");
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}...`);
-})
